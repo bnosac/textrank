3e3c987d2702db7c53ab9e57fc595c5bfed84df4
Jan Wijffels <jwijffels@bnosac.be>
2019-01-14 12:40:46 +0100

    Incorporate suggestion in #6

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+# CHANGES IN textrank VERSION 0.3.0
+
+- Speedup textrank_candidates_all (thanks to @emillykkejensen)
+- Make sure in the help a sentence identifier is used explicitely when doing data preparation for textrank_sentences
+
 # CHANGES IN textrank VERSION 0.2.0
 
 - Allow to extract keywords for summarizing text by adding textrank_keywords
--- R/textrank.R
+++ R/textrank.R
@@ -33,13 +33,15 @@ textrank_jaccard <- function(termsa, termsb){
 #' @seealso \code{\link{textrank_sentences}}
 #' @examples
 #' library(textreuse)
+#' library(udpipe)
 #' lsh_probability(h = 1000, b = 500, s = 0.1) # A 10 percent Jaccard overlap will be detected well
 #'
 #' minhash <- minhash_generator(n = 1000, seed = 123456789)
 #'
 #' data(joboffer)
-#' terminology <- subset(joboffer, upos %in% c("NOUN", "ADJ"), select = c("sentence_id", "lemma"))
-#' candidates <- textrank_candidates_lsh(x = terminology$lemma, sentence_id = terminology$sentence_id,
+#' joboffer$textrank_id <- unique_identifier(joboffer, c("doc_id", "paragraph_id", "sentence_id"))
+#' terminology <- subset(joboffer, upos %in% c("NOUN", "ADJ"), select = c("textrank_id", "lemma"))
+#' candidates <- textrank_candidates_lsh(x = terminology$lemma, sentence_id = terminology$textrank_id,
 #'                                       minhashFUN = minhash, bands = 500)
 #' head(candidates)
 textrank_candidates_lsh <- function(x, sentence_id, minhashFUN, bands){
@@ -85,16 +87,18 @@ textrank_candidates_lsh <- function(x, sentence_id, minhashFUN, bands){
 #' @export
 #' @seealso \code{\link{textrank_sentences}}
 #' @examples
+#' library(udpipe)
 #' data(joboffer)
-#' candidates <- textrank_candidates_all(unique(joboffer$sentence_id))
+#' joboffer$textrank_id <- unique_identifier(joboffer, c("doc_id", "paragraph_id", "sentence_id"))
+#' candidates <- textrank_candidates_all(unique(joboffer$textrank_id))
 #' head(candidates, 50)
 textrank_candidates_all <- function(x){
   x <- unique(x)
   x <- setdiff(x, NA)
-  candidates <- utils::combn(x = x, m = 2, simplify = FALSE)
-  candidates <- lapply(candidates, FUN=function(x){
-    list(textrank_id_1 = x[1],
-         textrank_id_2 = x[2])
+  x_length <- length(x)
+  stopifnot(x_length > 1)
+  candidates <- lapply(seq(x)[-x_length], function(i){
+    data.table::data.table(textrank_id_1 = x[i], textrank_id_2 = x[(i+1L):x_length])
   })
   candidates <- data.table::rbindlist(candidates)
   candidates <- setDF(candidates)
@@ -145,12 +149,13 @@ textrank_candidates_all <- function(x){
 #' }
 #' @export
 #' @examples
+#' library(udpipe)
 #' data(joboffer)
 #' head(joboffer)
-#'
-#' sentences <- unique(joboffer[, c("sentence_id", "sentence")])
+#' joboffer$textrank_id <- unique_identifier(joboffer, c("doc_id", "paragraph_id", "sentence_id"))
+#' sentences <- unique(joboffer[, c("textrank_id", "sentence")])
 #' cat(sentences$sentence)
-#' terminology <- subset(joboffer, upos %in% c("NOUN", "ADJ"), select = c("sentence_id", "lemma"))
+#' terminology <- subset(joboffer, upos %in% c("NOUN", "ADJ"), select = c("textrank_id", "lemma"))
 #' head(terminology)
 #'
 #' ## Textrank for finding the most relevant sentences
@@ -161,7 +166,7 @@ textrank_candidates_all <- function(x){
 #' ## Using minhash to reduce sentence combinations - relevant if you have a lot of sentences
 #' library(textreuse)
 #' minhash <- minhash_generator(n = 1000, seed = 123456789)
-#' candidates <- textrank_candidates_lsh(x = terminology$lemma, sentence_id = terminology$sentence_id,
+#' candidates <- textrank_candidates_lsh(x = terminology$lemma, sentence_id = terminology$textrank_id,
 #'                                       minhashFUN = minhash, bands = 500)
 #' tr <- textrank_sentences(data = sentences, terminology = terminology,
 #'                          textrank_candidates = candidates)
